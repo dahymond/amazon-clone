488cbfd68a5ff6451abea756b88fd86d0aba56d3
Solomon Sunday <ssdahymond@gmail.com>
2020-10-18 00:05:08 +0100

    commit the react setup

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDGV4-QQBnwXo20A7FymAGXcGI--kg1Yuw",
+    authDomain: "clone-5fb16.firebaseapp.com",
+    databaseURL: "https://clone-5fb16.firebaseio.com",
+    projectId: "clone-5fb16",
+    storageBucket: "clone-5fb16.appspot.com",
+    messagingSenderId: "250012374145",
+    appId: "1:250012374145:web:102de4e1e296cab623a980"
+  };
