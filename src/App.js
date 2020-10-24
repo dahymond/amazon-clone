3492b1633828d4873a590ad6a33885f6be8e3602
Solomon Sunday <ssdahymond@gmail.com>
2020-10-25 00:58:41 +0100

    initialised the checkout page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home  from './Home'
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
     //BEM
-    <div className="App">
-      <Header/>
-      <Home />
-    </div>
+    <Router>
+     <div className="App">
+       <Switch>
+         <route path="/checkout">
+           <Header/>
+           <h1>Iam the checkout page, please hit the smash button</h1>
+         </route>
+         <route path="/">
+           <Header />
+           <Home />
+         </route>
+       </Switch>
+     </div>
+    </Router>
   );
 }
 
--- src/Product.js
+++ src/Product.js
@@ -21,7 +21,7 @@ function Product({title, image, price, rating}) {
             </div>
 
             <img
-              src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+              src={image}
               alt=""
             />
             <button>Add to Basket</button>
