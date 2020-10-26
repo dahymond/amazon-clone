8627b620fd5fb07927ae3ffc67b1b5a939afdf0b
Solomon Sunday <ssdahymond@gmail.com>
2020-10-26 23:30:57 +0100

    adding state to the checkout product

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Subtotal.css';
 import CurrencyFormat from "react-currency-format";
-import ShoppingBasket from './ShoppingBasket';
+import ShoppingBasket from './CheckoutProduct';
 import { useStateValue } from './StateProvider';
 import { getBasketTotal } from './reducer';
 
